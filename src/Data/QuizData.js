b02b26d9576414056b13fb8d8617a3e8b47df4c5
= <=>
2023-08-20 23:59:46 +0530

    dsf

---
+++ src/Data/QuizData.js
@@ -0,0 +1,52 @@
+export const QuizData = [
+    {
+        question : 'What is React.js primarily used for ?',
+        options : [ "Data storage", "Styling", "User interface development", "Server-side scripting"],
+        answer : 3
+    },
+    {
+        question : 'Which of the following is NOT a built-in API in Node.js?',
+        options : [ "EventEmitter", "Buffer", "Console", "Navigator"],
+        answer : 4
+    },
+    {
+        question : 'Which of the following is used to create a new React element ?',
+        options : [ "createElement()", "createNode()", "createComponent()", "newElement()"],
+        answer : 1
+    },
+    {
+        question : 'In Node.js, which object represents the global scope?',
+        options : [ "global", "window", "document", "process"],
+        answer : 1
+    },
+    {
+        question : 'Which lifecycle method is called after a components output has been rendered to the DOM?',
+        options : [ "componentDidMount", "componentDidUpdate", "componentWillMount", "componentWillUnmount"],
+        answer : 2
+    },
+    {
+        question : 'Which React Hook is used to manage state in functional components?',
+        options : [ "useState", "useEffect", "useReducer", "useContext"],
+        answer : 1
+    },
+    {
+        question : 'In JSX, which syntax is used to embed JavaScript expressions?',
+        options : [ "{% ... %}", "{{ ... }}", "${ ... }", "{...}"],
+        answer : 3
+    },
+    {
+        question : 'Which of the following is not a core module in Node.js?',
+        options : [ "fs (File System)", "http (Hypertext Transfer Protocol)", "req (Request)", "os (Operating System)"],
+        answer : 3
+    },
+    {
+        question : 'In Node.js, which module is used for reading and writing files?',
+        options : [ "url", "querystring", "fs (File System)", "http"],
+        answer : 3
+    },
+    {
+        question : 'What is the default package manager that comes with Node.js?',
+        options : [ "Yarn", "NPM (Node Package Manager)", "Pip", "Composer"],
+        answer : 2
+    }
+]
